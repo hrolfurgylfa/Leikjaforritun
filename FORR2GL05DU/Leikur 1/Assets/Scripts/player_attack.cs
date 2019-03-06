e67ac400cce3393cf8e1bf9c1d7e34bd89be9ba8
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2019-03-06 10:14:20 +0000

    attack bairly komid

---
+++ FORR2GL05DU/Leikur 1/Assets/Scripts/player_attack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_attack : MonoBehaviour
+{
+    public Animator animator;
+    float sla;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        animator = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sla = Input.GetAxis ("Fire1");
+        animator.SetFloat("sla",sla);
+    }
+}
