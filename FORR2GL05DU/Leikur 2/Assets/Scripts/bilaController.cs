a066c88b1b8ab015f75b8c2a2ff8190c4af5c050
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2019-05-13 00:31:30 +0000

    comment komin í bíla controllerinn

--- FORR2GL05DU/Leikur 2/Assets/Scripts/bilaController.cs
+++ FORR2GL05DU/Leikur 2/Assets/Scripts/bilaController.cs
@@ -9,7 +9,7 @@ public class bilaController : MonoBehaviour {
 
     public int mestiHradi;
     public int mestiKraftur;
-    public WheelJoint2D[] dekkjaTengingar;
+    public WheelJoint2D[] dekkjaTengingar;// Þetta er breytanlegur listi í UIinu
     public GameObject stigaGeymsla;
 
     public string stigaTexti;
@@ -25,8 +25,8 @@ public class bilaController : MonoBehaviour {
     void Start() {
         // JointMotor2D motor = new JointMotor2D {motorSpeed = mestiHradi, maxMotorTorque = mestiKraftur};
         motorIGangi = false;
-        rb = GetComponent<Rigidbody2D>();
-        audioData = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody2D>();// Þetta finnur rigidbodyinn á gameObjectinum
+        audioData = GetComponent<AudioSource>();// Þetta finnur hljóð spilarann á gameObjectinum
 
         // Finna stigageymslu scriptið
         stigaScript = stigaGeymsla.GetComponent<stigaGeymsla>();
@@ -37,33 +37,31 @@ public class bilaController : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetAxis("Btn1") != 0 && motorIGangi == false) {
-            Debug.Log("Motor settur í gang");
+        if (Input.GetAxis("Btn1") != 0 && motorIGangi == false) {// Þetta kveikir á mótornum ef það er ekki kveikt á honum núþegar og það er haldið inni Btn1
             int tel = 0;
-            while (tel != dekkjaTengingar.Length) {
+            while (tel != dekkjaTengingar.Length) {// Þetta keyrir í gegnum allar dekkja tengingarnar og kveikir á öllum mótorunum
                 dekkjaTengingar[tel].useMotor = true;
                 tel += 1;
             }
             motorIGangi = true;
-            audioData.Play(0);
-        } else if (Input.GetAxis("Btn1") == 0 && motorIGangi == true) {
-            Debug.Log("Slökkt á motor");
+            audioData.Play(0);// Þetta spilar bíla hljóðið
+        } else if (Input.GetAxis("Btn1") == 0 && motorIGangi == true) {// Þetta slekkur á mótornum ef það er ekki slökkt á honum núþegar og það er sleppt Btn1
             int tel = 0;
-            while (tel != dekkjaTengingar.Length) {
+            while (tel != dekkjaTengingar.Length) {// Þetta keyrir í gegnum allar dekkja tengingarnar og slekkur á öllum mótorunum
                 dekkjaTengingar[tel].useMotor = false;
                 tel += 1;
             }
             motorIGangi = false;
-            audioData.Stop();
+            audioData.Stop();// Þetta stoppar bíla hljóðið
         }
 
-        if (Input.GetAxis("Btn1") != 0) {
+        if (Input.GetAxis("Btn1") != 0) {// Þetta bætir við snúningi ef það er haldið niðri Btn1
             rb.AddTorque(-100);
         }
 
-        int fjarlaegdNuna = (int)Math.Floor(Math.Abs(this.gameObject.transform.position.x));
-        if (fjarlaegdNuna > fjarlaegdFerdast) {
-            fjarlaegdFerdast = fjarlaegdNuna;
+        int fjarlaegdNuna = (int)Math.Floor(Math.Abs(this.gameObject.transform.position.x));// Þetta finnur núverandi fjarlægð frá núll punkti sem er notað sem stig, Abs er notað til þess að fá absolout gildi vegna þess að bílinn keyrir í áttina -x
+        if (fjarlaegdNuna > fjarlaegdFerdast) {// Þetta keyrir ef fjarlægðin núna frá byrjunarpunktinum er sú mesta sem hún hefur verið
+            fjarlaegdFerdast = fjarlaegdNuna;// Þetta uppfærir mestu fjarlægðina
 
             // Uppfæra skjá og geymslur
             stigaTextabox.text = stigaTexti + fjarlaegdFerdast;
