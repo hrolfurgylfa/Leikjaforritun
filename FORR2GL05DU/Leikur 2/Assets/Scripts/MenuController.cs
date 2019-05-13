17a9a0a37eee1e4bc274c59234f3ea116f43d7f0
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2019-05-13 00:17:37 +0000

    comment komin á menu controller

--- FORR2GL05DU/Leikur 2/Assets/Scripts/MenuController.cs
+++ FORR2GL05DU/Leikur 2/Assets/Scripts/MenuController.cs
@@ -6,19 +6,19 @@ using UnityEngine.UI;// Ég sæki þetta til þess að geta unnið með UI í sc
 
 public class MenuController : MonoBehaviour
 {
-    public void StartGame() {
-        GameObject StigaGeymsla = GameObject.Find("StigaGeymsla");
-        Destroy(StigaGeymsla);
+    public void StartGame() {// Þetta er keyrt af takkanum í Menu senuni
+        GameObject StigaGeymsla = GameObject.Find("StigaGeymsla");// Þetta nær í stiga geymsluna ef hún er til
+        Destroy(StigaGeymsla);// Þetta eyðir stigageymsluni svo að það sé hægt að búa til nýja
         SceneManager.LoadScene(1);// Þetta skiptir um senu og skiptir yfir í leikinn
     }
 
-    public Text stigSidastTextabox;
-    public string stigSidastTexti;
+    public Text stigSidastTextabox;// Hérna tek ég inn textabox
+    public string stigSidastTexti;// Hérna tek ég inn texta
 
     void Start() {
-        GameObject StigaGeymsla = GameObject.Find("StigaGeymsla");
-        if (StigaGeymsla != null) {
-            stigSidastTextabox.text = stigSidastTexti + StigaGeymsla.GetComponent<stigaGeymsla>().stig;
+        GameObject StigaGeymsla = GameObject.Find("StigaGeymsla");// Hérna reyni ég að finna gameObjectinn StigaGeymsla
+        if (StigaGeymsla != null) {// Þetta keyrir ef það er búið að spila allavegana eynu sinni, annars haldast leiðbeiningarnar um hvernig á að spila og sagan
+            stigSidastTextabox.text = stigSidastTexti + StigaGeymsla.GetComponent<stigaGeymsla>().stig;// Hérna breyti ég textanum í stigSidastTextabox í hversu mörg stig spilarinn fékk síðast
         }
     }    
 }
